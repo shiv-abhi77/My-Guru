861dba9cb984d228f25e676900c80952a856fdf7
N1ckool <104719138+Div7195@users.noreply.github.com>
2024-01-16 00:19:06 +0530

    common prefix urls for image link in both master and deployment branches

--- server/controllers/image-controllers.js
+++ server/controllers/image-controllers.js
@@ -1,7 +1,7 @@
 import grid  from "gridfs-stream";
 import mongoose from "mongoose";
 import Chat from "../model/chat-schema.js";
-const url = 'http://localhost:8000'
+const url = 'https://career-junction.vercel.app'
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
